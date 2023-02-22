403b1baf3d57e5aed80428773c4173231a51146f
ofek121 <54708885+ofek121@users.noreply.github.com>
2023-02-22 12:06:08 +0200

    fix: ssh_key

--- install_ssh_key.bash
+++ install_ssh_key.bash
@@ -12,5 +12,5 @@ for node in $(cat $hosts_file_path); do
         continue
     fi
     # do cat to public key file and copy to remote host using ssh
-    cat ~/.ssh/id_rsa.pub | ssh $username@$node "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+    cat /root/.ssh/id_rsa.pub | ssh $username@$node "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 done

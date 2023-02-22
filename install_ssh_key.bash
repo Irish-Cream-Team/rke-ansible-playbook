f7678e8f8c30e0d3cf4cf41c237dbe181369f94e
ofek121 <54708885+ofek121@users.noreply.github.com>
2023-02-22 12:27:31 +0200

    fix: add password

--- install_ssh_key.bash
+++ install_ssh_key.bash
@@ -5,12 +5,12 @@ ssh-keygen -t rsa -b 2048 -f /root/.ssh/id_rsa -q -N ""
 
 username="adminz"
 hosts_file_path="hosts"
-
+password=$1
 for node in $(cat $hosts_file_path); do
     # if node as the value of [nodes] then skip
     if [ $node == "[nodes]" ]; then
         continue
     fi
     # do cat to public key file and copy to remote host using ssh
-    cat /root/.ssh/id_rsa.pub | ssh $username@$node "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+    cat /root/.ssh/id_rsa.pub | sshpass -p $password ssh $username@$node "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
 done
